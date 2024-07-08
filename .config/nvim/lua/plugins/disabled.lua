24a40b4e739a49878403e4ba470f3f2fb9f253e5
shygerlai <ethan.418.63@gmail.com>
2024-07-08 17:25:11 +0800

    WSL migrate

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -1,5 +1,5 @@
 return {
     { "echasnovski/mini.pairs", enabled = false },
     { "stevearc/conform.nvim",  enabled = false },
-   -- { "folke/tokyonight.nvim",  enabled = false },
+    { "nvim-neo-tree/neo-tree.nvim", enabled = false },
 }
