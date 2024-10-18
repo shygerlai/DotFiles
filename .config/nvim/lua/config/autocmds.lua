94cf87de966961bff10f701daf51ef6522ee01a6
shygerlai <ethan.418.63@gmail.com>
2024-10-18 15:22:19 +0800

    update neovim

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,20 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+if vim.g.neovide then
+	vim.o.guifont = "FiraCode Nerd Font:h12"
+end
+vim.g.autoformat = false
+vim.o.tabstop = 4
+vim.o.expandtab = true
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+
+vim.cmd([[
+inoremap {<CR> {<CR>}<Esc>O
+nnoremap <C-d> <C-d>zz
+nnoremap <C-u> <C-u>zz
+set cindent
+set smartindent
+]])
--- .config/nvim/lua/plugins/vimtex.lua
+++
@@ -1,6 +0,0 @@
-return{
-    "lervag/vimtex",
-    lazy = false,
-    init = function()
-    end
-}
